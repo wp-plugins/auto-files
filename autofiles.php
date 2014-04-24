9339adf6be5863984564ecbb3ee6a3b667e1299a
kaisercrazy <kaisercrazy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-24 06:30:59 +0000

    Plugin released (24.04.2014)
    git-svn-id: https://plugins.svn.wordpress.org/auto-files/trunk@901482 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ autofiles.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Plugin Name: Auto Files
+Plugin URI: #
+Description: This plugin is minified version of Auto Attachments. Supported attachment types are Word, Excel, Pdf, PowerPoint, zip, rar, tar, tar.gz 
+Version: 0.1
+Author: Serkan Algur
+Author URI: http://www.wpadami.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+// Stop direct call
+if (preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) {
+				die('You are not allowed to call this page directly.');
+}
+
+// CSS Style Loading 
+if (!is_admin()){
+	wp_enqueue_style('autofilesstyle', plugins_url('/auto-files/autofiles.css'), __FILE__ );
+}
+
+
+function autof_show_files() {
+$files = get_children(array( //do only if there are attachments of these qualifications
+		'post_parent' => get_the_ID(),
+		'post_type' => 'attachment',
+		'numberposts' => -1,
+		'post_mime_type' => array(
+						"application/pdf",
+						"application/rar",
+						"application/msword",
+						"application/vnd.ms-powerpoint",
+						"application/vnd.ms-excel",
+						"application/zip",
+						"application/x-rar-compressed",
+						"application/x-tar",
+						"application/x-gzip",
+						"application/vnd.oasis.opendocument.text",
+						"application/vnd.oasis.opendocument.spreadsheet",
+						"application/vnd.oasis.opendocument.formula",
+						"text/plain",
+						"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+						"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+						"application/vnd.openxmlformats-officedocument.presentationml.presentation",
+						"application/vnd.openxmlformats-officedocument.presentationml.slideshow",
+						"application/x-compress",
+						"application/mathcad",
+						"application/postscript",
+						"applicationvnd.ms-excel.sheet.macroEnabled.12",
+		)));
+	if ($files) {
+		$filehtml .= '<div class="files section group">';
+		foreach ($files as $file) //setup array for more than one file attachment
+		{
+			$file_link       = wp_get_attachment_url($file->ID); //get the url for linkage
+			$file_name_array = explode("/", $file_link);
+			$file_post_mime  = str_replace("/", "-", $file->post_mime_type);
+			$file_post_mime  = str_replace(".", "-", $file_post_mime);
+			$file_name       = array_reverse($file_name_array); //creates an array out of the url and grabs the filename
+			$filehtml .= "<div class='col span_1_of_6 filein' id='$file->ID'>";
+			$filehtml .= "<a href='$file_link' target='_blank'><i class='sfl-".$file_post_mime."'></i></a>";
+			$filehtml .= "<a class='filetitle' href='$file_link'><strong>" . $file->post_title . "</strong></a>";
+			$filehtml .= "</div>";
+		}
+		$filehtml .="</div>";		
+		$filehtml .="<div class='clear'> </div>";
+		return $filehtml;
+	} 
+}
+
+add_filter('the_content', 'autof_insertintoContent');
+function autof_insertintoContent($content) {
+		if (get_post_type() == 'post') {
+			if (!post_password_required()){ 
+				$content .= autof_show_files();
+				return $content;
+			} else {
+				return $content;
+			}
+		}
+		
+		if (get_post_type() == 'page'){
+			if (!post_password_required()){
+				$content .= autof_show_files();
+				return $content;
+			} else {
+				return $content;
+			}
+		}
+}
---
+++ readme.txt
@@ -0,0 +1,39 @@
+﻿=== Auto Files ===
+Contributors: kaisercrazy
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=HFYQEQFUBJ2RE&lc=TR&item_name=Auto%20Attachments%20Donation&currency_code=TRY&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
+Tags: attachments, attachment, zip, rar, tar, tar.gz, mp3, player, flv, mp4, mpg, odf, ods, plugin, documents, excel, files, pdf, spreadsheet, text, word
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Requires at least: 3.6
+Tested up to: 3.9
+Stable tag: trunk
+
+This plugin is minified version of Auto Attachments. Supported attachment types: Word, Excel, Pdf, PowerPoint, odf, ods, zip, rar, tar
+
+== Description ==
+
+Auto Files is a minified version of Auto Attachments. This Plugin show you attached files in your <code>the_content()</code>. Supported file types are doc, docx, xls, xlsx, zip, rar, ods, ppt, odt, tar,  etc...
+Plugin uses own file icon font. See screenshot
+
+== Installation ==
+
+1. Upload `auto-files` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= How i upload files to my blog or posts? =
+Use WordPress media uploader when you create new page or post **(Insert / Upload button)** and upload your files. No special things required. Plugin uses wordpress attachments.
+
+
+== Screenshots ==
+
+1. Files Section
+
+== Changelog ==
+
+= Version 0.1 =
+* Plugin Released (24.04.2014)
+
+== Upgrade Notice ==
+No need any changes
